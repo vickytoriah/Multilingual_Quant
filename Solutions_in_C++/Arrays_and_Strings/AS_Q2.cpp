2e171b42ee78b8263442fb139ee5e900ba962e8f
vickytoriah <115563431+vickytoriah@users.noreply.github.com>
2024-09-28 21:20:49 +0100

    completed the cpp

---
+++ Solutions_in_C++/Arrays_and_Strings/AS_Q2.cpp
@@ -0,0 +1,23 @@
+//
+// Created by victo on 12 Sep 2024.
+//
+/* Answer to Merge Strings alternatively in Arrays and strings
+
+ */
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+class Solution {
+	/* You are given two strings word1 and word2.
+	Merge the strings by adding letters in alternating order, starting with word1.
+	If a string is longer than the other, append the additional letters onto the end of the merged string.
+	Return the merged string.
+	 */
+
+public:
+ string mergeAlternately(string word1, string word2) {
+
+ }
+};
